f51186f31b150c84d70427acc8599e410e090f26
Robin <weiserfi@outlook.com>
2018-06-18 01:27:39 -0600

    tsm test

---
+++ tsm.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include <mpi.h>
+ 
+
+int completado[10], costo, res, rank, size;
+
+int ciudades = 8;
+int distancias[8][8] = {
+{0 , 115, 8 , 17 , 167, 26 ,83, 75},
+{115, 0, 120, 129, 272, 92, 197, 100},
+{8, 120, 0, 9, 160, 34, 78, 83},
+{17, 129, 9, 0, 151, 43, 69, 91},
+{167, 272, 160, 151, 0, 193, 98, 236},
+{26, 92, 34, 43, 193, 0, 108, 55 },
+{83, 197, 78, 69, 98, 108, 0, 141},
+{75, 100, 83, 91, 236, 55, 141, 0}
+};
+
+
+int least(int c)
+{
+    int i,nc=999;
+    int min=999,kmin;
+ 
+    for(i=0;i < ciudades;i++)
+    {
+        if((distancias[c][i]!=0)&&(completado[i]==0))
+            if(distancias[c][i]+distancias[i][c] < min)
+            {
+                min=distancias[i][0]+distancias[c][i];
+                kmin=distancias[c][i];
+                nc=i;
+            }
+    }
+ 
+    if(min!=999)
+        costo+=kmin;
+ 
+    return nc;
+}
+
+void mincosto(int city)
+{
+    int i,ncity;
+ 
+    completado[city]=1;
+ 
+    printf("%d--->",city+1);
+    ncity=least(city);
+ 
+    if(ncity==999)
+    {
+        ncity=0;
+        printf("%d",ncity+1);
+        costo+=distancias[city][ncity];
+ 
+        return;
+    }
+ 
+    mincosto(ncity);
+}
+
+ 
+int main(int argc, char** argv)
+{
+ 
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+//	MPI_Send();
+//	MPI_Reccv();
+
+
+    printf("\n\nEl recorrido es:\n");
+    mincosto(0);
+ 
+    printf("\n\nLa distancia minima total es: %d\n ",costo);
+ 
+	MPI_Finalize();
+    return 0;
+}
